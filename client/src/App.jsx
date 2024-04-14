1ba792cd951cbca4af6c655dd2cce5af3a939a9d
Gita Neupane <gitaneupane88@gmail.com>
2024-04-14 07:52:01 +0545

    Created pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Dashboard from "./pages/Dashboard"
+import Projects from "./pages/Projects"
+import Signin from "./pages/Signin"
+import Signup from "./pages/Signup"
 
 const App = () => {
   return (
-    <div>
-      <h1 className='text-red-300'> MERN STACK</h1>
-    
-    </div>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/sign-in' element={<Signin/>}/>
+        <Route path='/sign-up' element={<Signup/>}/>
+        <Route path='/dashboard' element={<Dashboard/>}/>
+        <Route path='/projects' element={<Projects/>}/>
+      </Routes>
+      </BrowserRouter>
   )
 }
 
