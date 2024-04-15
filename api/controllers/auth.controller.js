6cf674074425d5cb2a1910b7bbc266c06d9cb898
Gita Neupane <gitaneupane88@gmail.com>
2024-04-15 08:44:03 +0545

    added middleware functions to handle errors!

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,11 +1,11 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
 
-export const signup=async (req,res)=>{
+export const signup=async (req,res, next)=>{
     const {username, password, email}= req.body;
 
     if(!username || !email || !password || username==='' || email==='' ||password===''){
-        return res.status(400).json({message:"All fields are required!"});
+     next(errorHandler(400, "All fields are required!!"));
     }
 
 
@@ -23,7 +23,7 @@ export const signup=async (req,res)=>{
     res.json({message: "Signup successful!"});
     }
     catch(error){
-        res.status(500).json({message:error.message})
+       next(error);
     }
 }
 
--- api/index.js
+++ api/index.js
@@ -32,5 +32,13 @@ app.use('/api/user',userRoutes );
 app.use('/api/auth', authRoutes);                                       //Api testing
 
 
-
+app.use((err, req, res,next)=>{
+    const statusCode=err.statusCode || 500;
+    const message=err.message || "Internal Server error!";
+    res.status(statusCode.json({
+     success:false,
+     statusCode,
+     message,
+    }))
+});
 
